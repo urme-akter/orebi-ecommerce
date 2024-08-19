15cf56d0cad63acde5b641c1f879fc4928d81a42
urme-akter <112346871+urme-akter@users.noreply.github.com>
2024-08-19 17:05:23 +0600

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './index.css'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>start</h1>
-
+      <h1 className="bg-orange-400">start</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
